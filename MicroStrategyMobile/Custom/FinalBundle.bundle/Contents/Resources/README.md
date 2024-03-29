c4acbcd60b7fa53278aa742dcca4ff193c6cd1af
Wiliams Jiménez <64094509+bi-factory@users.noreply.github.com>
2024-03-28 23:40:06 -0500

    Delete MicroStrategyMobile directory

--- MicroStrategyMobile/Custom/FinalBundle.bundle/Contents/Resources/README.md
+++
@@ -1,1 +0,0 @@
-# this folder is used to store customized resources including icons, images, localization files which will be used to replace the application's resources
--- MicroStrategyMobile/Resolve.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Resolve.swift
-//  MicroStrategyMobile
-//
-//  Created by Xie, Dongze on 2020/12/22.
-//  Copyright © 2020 MicroStratgy Inc. All rights reserved.
-//
-
-import Foundation
